51c216ddbbff1147f2ac7089a59568d96f331b9c
valentinbegnis <begnisvalentin@gmail.com>
2023-05-29 15:20:57 -0300

    Change key values to avoid using index

--- components/ShootingStars.tsx
+++ components/ShootingStars.tsx
@@ -8,7 +8,7 @@ export default function ShootingStars() {
   return (
     <div className="shooting-stars-animation">
       {(pathname === '/' || pathname === '/contact')
-        && [...new Array(11)].map((_, i) => <span key={i} />)}
+        && Array.from(Array(10), (x, i) => i + 1).map((starNumber) => <span key={starNumber} />)}
     </div>
   );
 }
